7de1e3162b653506d9416890ff61213f417590bd
alstjr5949 <alstjr5949@gmail.com>
2022-06-30 00:03:32 +0900

    data fetch in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import BlogDetail from "./pages/BlogDetail/BlogDetail";
 import Home from "./pages/Home/Home";
 
 function App() {
+  const [blogData, setBlogData] = useState({});
+  const [userData, setUserData] = useState([]);
+
+  const fetchData = async () => {
+    const data = await (await fetch("data.json")).json();
+    setBlogData(data);
+    setUserData(data.users[0]);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route
+        path="/"
+        element={<Home blogData={blogData} userData={userData} />}
+      />
+      <Route path="/blog/:id" element={<BlogDetail blogData={blogData} />} />
     </Routes>
   );
 }
